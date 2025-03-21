0c40aba50a0dcaf88b3ddc6e34c34b55916740e7
Abdullah <49422693+aluay@users.noreply.github.com>
2025-03-21 00:16:37 -0700

    Delete prisma/migrations directory

--- prisma/migrations/20250227081743_fix_reminder_applicationid/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `applicationId` on table `Notification` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Notification" ALTER COLUMN "applicationId" SET NOT NULL;
